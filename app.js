b7f53d0ddfef856ee68a434e9d88eaa3517b3538
Joseph Alba <jalba19@gmail.com>
2018-10-15 10:32:28 -0400

    adding functionality

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
     // Get value of input
-    let filterValue = document.getElementById('filterInput').nodeValue.toUpperCase();
-    console.log(filterValue);
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
+    // Get names uL
+    let ul = document.getElementById('names');
+
+    // Get Li's from uL
+    let li = ul.querySelectorAll('li.collection-item');
+
+    // Loop through collection-item li's
+    for (let i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];
+
+        // If matched
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > - 1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
 }
